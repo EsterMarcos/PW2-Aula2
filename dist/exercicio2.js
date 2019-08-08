be8e9a2d9d604a3faf36c9db531d895d8164dcc3
EsterMarcos <estermarcoshas.com>
2019-08-07 22:26:45 -0300

    Exercicio de doação de sangue

---
+++ dist/exercicio2.js
@@ -0,0 +1,9 @@
+{
+    var idade = 14;
+    if (idade >= 18 && idade <= 67) {
+        console.log("Sim, você pode ser doador(a) de sangue");
+    }
+    else {
+        console.log("Não, você não pode ser doador(a) de sangue");
+    }
+}
---
+++ exercicio2.ts
@@ -0,0 +1,14 @@
+{
+    let  idade : number = 14;
+    
+
+    if(idade>=18 && idade<=67)
+    {
+        console.log ( "Sim, você pode ser doador(a) de sangue");
+    }
+    else
+    {
+        console.log ( "Não, você não pode ser doador(a) de sangue");
+    }
+
+}
